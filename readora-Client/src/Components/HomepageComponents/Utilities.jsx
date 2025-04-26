58868560d01ccd99bcf065cf15f9ea7210d58289
Gaurav <gauravkrdas19@gmail.com>
2025-04-26 11:24:18 +0530

    logo added

--- readora-Client/src/Components/HomepageComponents/Utilities.jsx
+++ readora-Client/src/Components/HomepageComponents/Utilities.jsx
@@ -3,13 +3,13 @@ import { HiUserCircle } from "react-icons/hi";
 import { FaHome } from "react-icons/fa";
 import { CgFeed } from "react-icons/cg";
 import { IoLogOut } from "react-icons/io5";
+import Logo from '../../Logo';
 
 const Utilities = () => {
     return (
-        <div 
-        className="hidden flex-1 h-full justify-center pt-10 lg:flex"
-        >
-            <div className="sticky w-5/6 h-fit bg-gunMetal p-4 rounded-xl">
+        <div className="hidden flex-1 h-full items-center pt-10 lg:flex flex-col">
+            <Logo />
+            <div className="sticky w-8/9 h-fit bg-gunMetal p-4 rounded-xl mt-6">
                 <ul className='flex flex-col gap-3'>
                     <li className='p-2 text-2xl text-babyPowder font-semibold cursor-pointer rounded-md hover:bg-zinc-700' onClick={()=>console.log("account")}>
                         <div className='flex gap-2 items-center'>
