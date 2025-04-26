e5cc35fc27fac0b591000829e3613336ab042976
Gaurav <gauravkrdas19@gmail.com>
2025-04-26 11:34:01 +0530

    utility hover effect changed

--- readora-Client/src/Components/HomepageComponents/FriendList.jsx
+++ readora-Client/src/Components/HomepageComponents/FriendList.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const FriendList = () => {
     return (
-        <div className="flex-1 h-full bg-tomato"></div>
+        <div className="hidden flex-1 h-full items-center pt-10 lg:flex flex-col">
+            <input 
+                type="text" 
+                placeholder="Search friends..." 
+                className="w-8/9 p-2 text-lg rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-azul bg-white mt-4"
+            />
+        </div>
     )
 }
 
--- readora-Client/src/Components/HomepageComponents/Utilities.jsx
+++ readora-Client/src/Components/HomepageComponents/Utilities.jsx
@@ -11,25 +11,25 @@ const Utilities = () => {
             <Logo />
             <div className="sticky w-8/9 h-fit bg-gunMetal p-4 rounded-xl mt-6">
                 <ul className='flex flex-col gap-3'>
-                    <li className='p-2 text-2xl text-babyPowder font-semibold cursor-pointer rounded-md hover:bg-zinc-700' onClick={()=>console.log("account")}>
+                    <li className='p-2 text-2xl text-babyPowder font-semibold cursor-pointer rounded-md hover:bg-tomato hover:text-gunMetal' onClick={()=>console.log("account")}>
                         <div className='flex gap-2 items-center'>
                             <HiUserCircle />
                             Account
                         </div>
                     </li>
-                    <li className='p-2 text-2xl text-babyPowder font-semibold cursor-pointer rounded-md hover:bg-zinc-700' onClick={()=>console.log("home")}>
+                    <li className='p-2 text-2xl text-babyPowder font-semibold cursor-pointer rounded-md hover:bg-tomato hover:text-gunMetal' onClick={()=>console.log("home")}>
                         <div className='flex gap-2 items-center'>
                             <FaHome />
                             Home
                         </div>
                     </li>
-                    <li className='p-2 text-2xl text-babyPowder font-semibold cursor-pointer rounded-md hover:bg-zinc-700' onClick={()=>console.log("feed")}>
+                    <li className='p-2 text-2xl text-babyPowder font-semibold cursor-pointer rounded-md hover:bg-tomato hover:text-gunMetal' onClick={()=>console.log("feed")}>
                         <div className='flex gap-2 items-center'>
                             <CgFeed />
                             Feed
                         </div>
                     </li>
-                    <li className='p-2 text-2xl text-babyPowder font-semibold cursor-pointer rounded-md hover:bg-zinc-700' onClick={()=>console.log("logout")}>
+                    <li className='p-2 text-2xl text-babyPowder font-semibold cursor-pointer rounded-md hover:bg-tomato hover:text-gunMetal' onClick={()=>console.log("logout")}>
                         <div className='flex gap-2 items-center'>
                             <IoLogOut />
                             Log Out
