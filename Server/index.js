bc7544a2718207628d580451cd687c9280245322
Gaurav <gauravkrdas19@gmail.com>
2025-05-01 23:20:37 +0530

    mongoDB connected

--- Server/index.js
+++ Server/index.js
@@ -6,6 +6,11 @@ const helmet = require('helmet');
 const morgan = require('morgan');
 
 dotenv.config()
+
+mongoose.connect(process.env.MONGO_URL)
+    .then(() => console.log("MongoDB connected"))
+    .catch((err) => console.error(err));
+
 app.use(express.json())
 app.use(morgan('tiny'))
 app.use(helmet())
