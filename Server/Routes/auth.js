c5b6b894d17ce0c69d2b4d5754e0d2608ada1e04
Gaurav <gauravkrdas19@gmail.com>
2025-05-02 21:01:00 +0530

    login and register completed

--- Server/Routes/auth.js
+++ Server/Routes/auth.js
@@ -1,26 +1,62 @@
 const router = require('express').Router()
 const UserSchema = require('../Models/userSchema')
 
-router.get("/", (req, res) => {
-    res.send('auth route');
-})
-
 router.post("/register", async (req, res) => {
     const { name, username, email, password } = req.body;
     try {
         const newUser = new UserSchema({ name, username, email, password, })
         const savedUser = await newUser.save();
-        res.status(200).json({err:false,msg:"user saved successfully"})
+        res.status(200).json({
+            err: false,
+            msg: "user saved successfully"
+        })
         console.log(savedUser);
     } catch (err) {
         if (err.name === 'ValidationError') {
             const messages = Object.values(err.errors).map(val => val.message);
-            res.status(400).json({ err:true,msg: messages });
-          } else if (err.code === 11000) {
-            res.status(400).json({ err:true,msg: 'Username or email already exists' });
-          } else {
-            res.status(500).json({ err:true,msg: 'Server error' });
-          }
+            res.status(400).json({
+                err: true,
+                msg: messages
+            });
+        } else if (err.code === 11000) {
+            res.status(400).json({
+                err: true,
+                msg: 'Username or email already exists'
+            });
+        } else {
+            res.status(500).json({
+                err: true,
+                msg: 'Server error'
+            });
+        }
+    }
+});
+
+router.post("/login", async(req,res)=>{
+    const {username, password} = req.body;
+    try{
+        const user = await UserSchema.findOne({username});
+        if(!user) res.status(400).json({
+            isValidated: false,
+            err: false,
+            msg: "Invalid username or password",
+        })
+        if(user.password !== password) res.status(400).json({
+            isValidated: false,
+            err: false,
+            msg: "Incorrect password",
+        })
+
+        res.status(200).json({
+            isValidated: true,
+            err: false,
+            msg: user,
+        })
+    }catch(err){
+        res.status(500).json({
+            err: true,
+            msg: "Internal Server Error"
+        })
     }
 })
 
