1c0cc5924901574e310ed19763f03bf7bd06f3ba
Gaurav <gauravkrdas19@gmail.com>
2025-05-03 10:27:00 +0530

    get user posts

--- Server/Routes/posts.js
+++ Server/Routes/posts.js
@@ -27,8 +27,29 @@ router.post("/upload", async(req,res)=>{
 
 //edit post
 //delete post
-//like post
-//unlike post
+//like/unlike post
+//get all post by user
+router.get("/:id/profilePost", async(req,res)=>{
+    const profileID = req.params.id;
+    try{
+        const userPosts = await PostSchema.find({userId:profileID});
+        if(!userPosts) return res.status(400).json({
+            err: true,
+            msg: "No posts uploaded"
+        })
+
+        res.status(200).json({
+            err: false,
+            msg: userPosts,
+        })
+    } catch(err){
+        res.status(500).json({
+            err: true,
+            msg: "Error loading post"
+        })
+    }
+})
+
 //home feed post(by following users)
 
 module.exports = router
