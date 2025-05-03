21c18594b7bdfe5bbb30c034c685ff98146cb74b
Gaurav <gauravkrdas19@gmail.com>
2025-05-03 10:16:45 +0530

    post creation done

--- Server/Routes/posts.js
+++ Server/Routes/posts.js
@@ -1,11 +1,30 @@
 const router = require('express').Router()
 const PostSchema = require('../Models/postsSchema')
 
-router.get("/",(req,res)=>{
-    res.send("Post Route");
+router.get("/", (req, res) => {
+    res.send({
+        msg: "Post Route"
+    });
 })
 
 //create post
+router.post("/upload", async(req,res)=>{
+    try{
+        const post = new PostSchema(req.body)
+        const userPost = await post.save()
+        res.status(200).json({
+            err:false,
+            msg:"Post uploaded successfully"
+        })
+        console.log(userPost)
+    }catch(err){
+        res.status(500).json({
+            err:true,
+            msg: "Error uploading post"
+        })
+    }
+})
+
 //edit post
 //delete post
 //like post
