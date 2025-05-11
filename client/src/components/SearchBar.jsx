d55edb4bee171352346982d00181076ce339195f
Gaurav <gauravkrdas19@gmail.com>
2025-05-11 12:00:10 +0530

    getting the search value

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const SearchBar = ({ selected, handleSelection }) => {
+const SearchBar = ({ selected, handleSelection, onSearch, searchVal, setSearch }) => {
     return (
         <div className="bg-gunMetal flex flex-col h-full w-full rounded-2xl">
             <div className="flex items-center gap-2 p-4">
@@ -18,14 +18,16 @@ const SearchBar = ({ selected, handleSelection }) => {
                     Post
                 </p>
             </div>
-            <div className='px-4 pb-4 flex justify-between w-full gap-2'>
+            <form onSubmit={onSearch} className='px-4 pb-4 flex justify-between w-full gap-2'>
                 <input
                     type="text"
-                    placeholder={`Enter ${(selected === 0) ? 'username here' : 'search item'}...`}
+                    placeholder={`Enter ${(selected === 0) ? 'username here' : 'search item here'}...`}
                     className='outline-none placeholder:text-babyPowder/30 text-babyPowder w-10/12'
+                    value={searchVal}
+                    onChange={e=>setSearch(e.target.value)}
                 />
                 <button className='w-2/12 text-babyPowder font-semibold bg-azul hover:bg-azul/90 rounded-lg cursor-pointer py-1'>Search</button>
-            </div>
+            </form>
         </div>
     )
 }
--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -3,13 +3,19 @@ import SearchBar from '../components/SearchBar'
 
 const Search = () => {
     const [selected, setSelected] = useState(0)
+    const [search, setSearch] = useState('')
     const handleSelection = (selectionId) => {
         setSelected(selectionId)
+    }
+    const onSearch = (e)=>{
+        e.preventDefault()
+        console.log(search)
+        setSearch('')
     }
 
     return (
         <div className='mt-14'>
-            <SearchBar selected={selected} handleSelection={handleSelection}/>
+            <SearchBar selected={selected} handleSelection={handleSelection} onSearch={onSearch} setSearch={setSearch} searchVal={search}/>
         </div>
     )
 }
