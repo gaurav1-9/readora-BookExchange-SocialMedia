9ebf7f99a5d52755dff5e9911592f499333b8c66
Gaurav <gauravkrdas19@gmail.com>
2025-05-17 19:13:00 +0530

    do not show chats with no content

--- client/src/components/ChatList.jsx
+++ client/src/components/ChatList.jsx
@@ -8,61 +8,59 @@ const ChatList = ({ chats, user }) => {
         <ul>
             {
                 chats.map(chat => (
-                    <Link key={chat._id} to={`/msg/${chat._id}`}>
-                        <li className='flex gap-2 items-center bg-gunMetal/10 rounded-lg px-4 py-2 cursor-pointer hover:bg-gunMetal/14 mb-2'>
-                            <img src="/default-pic.png" alt="" className='w-15 rounded-full' />
-                            <div className="flex flex-col w-full">
-                                <div className="flex justify-between items-center">
-                                    {
-                                        chat.users.map((chatUser) => (
-                                            (chatUser._id.toString() === user._id)
-                                                ? null
-                                                : <p key={chatUser._id.toString} className='font-semibold text-xl text-gunMetal'>
-                                                    {chatUser.username}
-                                                </p>
-                                        ))
-                                    }
-                                    <div className='flex gap-2 font-semibold text-gunMetal/50 text-sm'>
-                                        <span>
-                                            {
-                                                new Date(chat.createdAt).toLocaleDateString('en-US', {
-                                                    year: 'numeric',
-                                                    month: 'long',
-                                                    day: 'numeric',
-                                                })
-                                            }
-                                        </span>
-                                        <span>|</span>
-                                        <span>
-                                            {
-                                                new Date(chat.createdAt).toLocaleTimeString('en-US', {
-                                                    hour: '2-digit',
-                                                    minute: '2-digit'
-                                                })
-                                            }
-                                        </span>
-                                    </div>
-                                </div>
-                                <div className="flex items-center gap-2">
-                                    {
-                                        (chat.latestMessage)
-                                            ? (chat.latestMessage.sender._id === user._id)
-                                                ? <GoArrowUpRight className='rounded-full text-xs' />
-                                                : <GoArrowDownLeft className='rounded-full text-xs' />
-                                            : null
-                                    }
-                                    <p className='text-md text-gunMetal/90'>
+                    (chat.latestMessage === undefined)
+                        ? null
+                        : <Link key={chat._id} to={`/msg/${chat._id}`}>
+                            <li className='flex gap-2 items-center bg-gunMetal/10 rounded-lg px-4 py-2 cursor-pointer hover:bg-gunMetal/14 mb-2'>
+                                <img src="/default-pic.png" alt="" className='w-15 rounded-full' />
+                                <div className="flex flex-col w-full">
+                                    <div className="flex justify-between items-center">
                                         {
-                                            (chat.latestMessage !== undefined)
-                                                ? chat.latestMessage.content
-                                                : <span className='invisible'>--</span>
+                                            chat.users.map((chatUser) => (
+                                                (chatUser._id.toString() === user._id)
+                                                    ? null
+                                                    : <p key={chatUser._id.toString} className='font-semibold text-xl text-gunMetal'>
+                                                        {chatUser.username}
+                                                    </p>
+                                            ))
                                         }
-                                    </p>
-                                </div>
+                                        <div className='flex gap-2 font-semibold text-gunMetal/50 text-sm'>
+                                            <span>
+                                                {
+                                                    new Date(chat.createdAt).toLocaleDateString('en-US', {
+                                                        year: 'numeric',
+                                                        month: 'long',
+                                                        day: 'numeric',
+                                                    })
+                                                }
+                                            </span>
+                                            <span>|</span>
+                                            <span>
+                                                {
+                                                    new Date(chat.createdAt).toLocaleTimeString('en-US', {
+                                                        hour: '2-digit',
+                                                        minute: '2-digit'
+                                                    })
+                                                }
+                                            </span>
+                                        </div>
+                                    </div>
+                                    <div className="flex items-center gap-2">
+                                        {
+                                            (chat.latestMessage)
+                                                ? (chat.latestMessage.sender._id === user._id)
+                                                    ? <GoArrowUpRight className='rounded-full text-xs' />
+                                                    : <GoArrowDownLeft className='rounded-full text-xs' />
+                                                : null
+                                        }
+                                        <p className='text-md text-gunMetal/90'>
+                                            {chat.latestMessage.content}
+                                        </p>
+                                    </div>
 
-                            </div>
-                        </li>
-                    </Link>
+                                </div>
+                            </li>
+                        </Link>
                 ))
             }
         </ul>
